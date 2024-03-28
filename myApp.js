276d8b11ae584adc1165bb305bd644482df1242c
Dinesh <dineshanbarasan2002@gmail.com>
2024-03-28 18:14:49 +0530

    fcc activity 3 done

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
 
 module.exports = app;
 const api = require('./server.js');
