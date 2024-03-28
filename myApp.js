51c25a638d58721f78f3da441a89511886957813
Dinesh <dineshanbarasan2002@gmail.com>
2024-03-28 18:03:20 +0530

    first commit

---
+++ myApp.js
@@ -0,0 +1,19 @@
+const helmet = require('helmet');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`FreeCodeCamp Test App is running on port ${port}`);
+});
